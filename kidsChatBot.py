fb241de96f64508725baab5b7cf0e8539d6f9e3b
Victor Sullivan <vssullivan@gmail.com>
2022-08-06 12:47:20 -0700

    Simple chatbot for kids.

---
+++ kidsChatBot.py
@@ -0,0 +1,20 @@
+# python3
+
+'''
+Simple input / output chatbot for kids.
+'''
+print("Hello, I am Zyxo 64. I am a chatbot.")
+print("I like animals and I love to talk about food.")
+name = input("What is your name? ")
+print("Hello, {}. Nice to meet you.".format(name))
+year = int(input("I'm not very good at dates, what is the year? \n(type in a number) "))
+print("I was created in {}.".format(year))
+food = input("What is your favorite food? \n(type in a food) ")
+print("I like {} too.".format(food))
+animal = input("What is your favorite animal? \n(type in a animal) ")
+print("I love {} too.".format(animal))
+print("I hope you enjoy talking to me!")
+print("Goodbye!")
+print("Zyxo 64")
+print("(c) 2022")
+print("All rights reserved.")
